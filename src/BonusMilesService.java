6309c8b60f4a135af447ee9d6b7247f835320171
Lose88 <hotsting1888@gmail.com>
2022-06-19 11:32:21 +0300

    initCommit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int miles;
+        int rubMiles = 20;
+        miles = cost / rubMiles;
+
+        return miles;
+    }
+
+}
